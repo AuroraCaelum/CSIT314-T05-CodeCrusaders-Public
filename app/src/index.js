508b9aa8b41aff5aaaf5f3404e32009f01cf07cc
NickyFH <nickyfabianho@gmail.com>
2024-10-21 20:53:08 +0800

    UserManagementUI update
    i made it more like the wireframe by removing the search bar and adding username, profile picture, change button name and logout button.

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LoginUI from './boundary/LoginUI'
+import LoginUI from './boundary/LoginUI';
+import UserManagementUI from './boundary/UserManagementUI';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <LoginUI />
+    <UserManagementUI />
   </React.StrictMode>
 );
 
