b7cac2f8fa98f3eebf9cabdba90b83b25596e4e8
kimdy <kimdy2395@gmail.com>
2024-11-09 17:55:34 +0800

    small update for User Profile to front and back connect
    login,
    User profile suspend, search

--- app/src/controller/UserProfileController.js
+++ app/src/controller/UserProfileController.js
@@ -64,11 +64,8 @@ class SuspendUserProfileController {
         try {
             const profile = new UserProfile(profileName, null, null); // Assuming constructor still needed
             const success = await profile.suspendUserProfile(profileName); // No need for profileId
-            if (success) {
-                return true;
-            } else {
-                return false;
-            }
+            console.log("Success to Suspend User Profile(Controller)", profileName);
+            return true;
         } catch (error) {
             return false;
         }
